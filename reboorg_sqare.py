4b3fc99fe074a8877132f56b239f57ef809e15c3
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-03 19:56:41 +0530

    Add files via upload

---
+++ reboorg_sqare.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+#draw square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+#run code on Reeborg's World
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
