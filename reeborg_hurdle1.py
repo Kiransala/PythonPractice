0c43e691cf03fd80db6f267976ce2a3b74a3002e
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-03 21:16:02 +0530

    Update reeborg_hurdle1.py

--- reeborg_hurdle1.py
+++ reeborg_hurdle1.py
@@ -11,12 +11,8 @@ def jump():
     turn_right()
     move()
     turn_left()
-jump()
-jump()
-jump()
-jump()
-jump()
-jump()
+for step in range(6):
+    jump()
 
 # run code at Reeborg's [World Hurdle 1]
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
