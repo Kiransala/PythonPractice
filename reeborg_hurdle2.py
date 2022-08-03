7a25406a703087f4ff026c55774168939f14d74d
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-03 20:48:22 +0530

    Add files via upload

---
+++ reeborg_hurdle2.py
@@ -0,0 +1,17 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while not at_goal():
+    jump()
+# run code at Reeborg's [World Hurdle 1]
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
