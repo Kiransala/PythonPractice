78ab967212b3f567741af87ea808d91282ba6d60
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-03 20:50:15 +0530

    Update reeborg_hurdle2.py

--- reeborg_hurdle2.py
+++ reeborg_hurdle2.py
@@ -13,5 +13,5 @@ def jump():
     turn_left()
 while not at_goal():
     jump()
-# run code at Reeborg's [World Hurdle 1]
+# run code at Reeborg's [World Hurdle 2]
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
