033f007e0ae49bb37af66b4bdb24f9a4fc77454d
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-08 20:13:07 +0530

    Update days_in_month.py

--- days_in_month.py
+++ days_in_month.py
@@ -11,8 +11,6 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
-    if month > 12 or month < 1:
-        return "Invalid month"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month > 12 or month < 1:
         return "Invalid month entered."
