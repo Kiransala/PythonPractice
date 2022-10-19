61531237a7080522646f5b23b80dab42a951fb52
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-10-19 20:15:07 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,127 @@
+# C =([[1, 2, 3, 4, 5], 
+#  [6, 7, 8, 9, 10], 
+#  [11, 12, 13, -14, 15], 
+#  [16, 17, 18, 19, 20], 
+#  [2, 2, 2, 2, 2]]) 
+
+# A=[]
+
+
+
+# for x in C:
+#     for number in x:
+#         if number == 7 :
+#             A.append(number)
+#         elif number== 8:
+#             A.append(number)
+            
+# print(A)
+
+# B=[]
+
+# for x in C:
+#     for number in x:
+#         if number == 1 :
+#             B.append(number)
+#         elif number== 6:
+#             B.append(number)
+#         elif number == 11:
+#             B.append(number)
+#         elif number == 16:
+#             B.append(number)
+            
+# print(B)
+
+# D=[]
+
+# for _ in C:
+    
+#     D.append(C[1][0])
+#     D.append(C[2][0])
+#     D.append(C[3][0])
+#     D.append(C[4][0])
+#     break
+    
+# print(D)
+
+# Obtain the elements [[8, 9], [13, -14]] from array C
+
+# A=[]
+# B=[]
+# for x in C:
+#     for number in x:
+#         if number == 8 :
+#             A.append(number)
+#         elif number== 9:
+#             A.append(number)
+#         elif number == 13:
+#             B.append(number)
+#         elif number == -14:
+#             B.append(number)
+# C=[]
+# C.append(A)
+# C.append(B)
+# print(C)
+# A=[]
+# D=[]
+# E=[]
+# for _ in C:
+#     D.append(C[0][1])
+#     D.append(C[0][4])
+#     E.append(C[3][1])
+#     E.append(C[3][4])
+#     break
+# A.append(D)
+# A.append(E)
+# print(A)
+
+# A=[]
+# D=[]
+# E=[]
+# for _ in C:
+#     D.append(C[0][1])
+#     D.append(C[0][2])
+#     D.append(C[0][3])
+#     E.append(C[4][1])
+#     E.append(C[4][2])
+#     E.append(C[4][3])
+#     break
+# A.append(D)
+# A.append(E)
+# print(A)
+
+# B = ( [ [10, 11, 12, 13],
+#  [20, 21, 22, 23],
+#  [30, 31, 32, 33],
+#  [40, 41, 42, 43] ] )
+ 
+ 
+ 
+# A=[]
+# D=[]
+# E=[]
+ 
+# for _ in B:
+#     D.append(B[1][1])
+#     D.append(B[1][2])
+#     E.append(B[2][1])
+#     E.append(B[2][2])
+#     break
+# A.append(D)
+# A.append(E)
+# print(A)
+ 
+# list1=[1,7,5,3,4]
+
+# for number in list1:
+#     if number ==3:
+#         print("Number 3 is present.")
+
+# for number in list1:
+#     list1[2]=21
+    
+# print(list1)
+
+# s = "Split this string on whitespaces"
+# words=s.split()
+# print(words)
